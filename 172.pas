5d94a0aa0c1a82c547fb2b33edf586ec9f1e2d15
algolGuru <61578859+algolGuru@users.noreply.github.com>
2020-04-05 13:58:23 +0300

    исправил замечания

--- 172.pas
+++ 172.pas
@@ -1,13 +1,14 @@
-PROGRAM ReadNumberApp(INPUT, OUTPUT);
+PROGRAM ReadNumberApp(INPUT, OUTPUT);{ReadNumberApp}
 VAR
- Over, Inv: BOOLEAN;
- Ch: CHAR;
- I, Num: INTEGER;
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
-BEGIN
-  D := 0;
-  READ(F, Ch);
+  Num: INTEGER;
+PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);{ReadDigit}
+VAR
+   Ch: CHAR;
+BEGIN{ReadDigit}
+  D := -1;{Если не встретится цирфа в файле, то процедура возвращает -1}
+  IF NOT(EOLN)
+  THEN
+    READ(F, Ch);  
   IF Ch = '1' THEN D := 1 ELSE
   IF Ch = '2' THEN D := 2 ELSE
   IF Ch = '3' THEN D := 3 ELSE
@@ -17,45 +18,39 @@ BEGIN
   IF Ch = '7' THEN D := 7 ELSE
   IF Ch = '8' THEN D := 8 ELSE
   IF Ch = '9' THEN D := 9 ELSE
-  IF Ch = '0' THEN D := 0 
-  ELSE
-    BEGIN
-      Ch := 'E'; //Значит считана не цифра 
-    END    
-END; 
-BEGIN
-  Inv := FALSE;
-  Over := FALSE;
-  WHILE NOT(EOLN(F)) AND (N >= 0)//Пока не конец строки 
+  IF Ch = '0' THEN D := 0;    
+END;{ReadDigit}  
+PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);{ReadNumber}
+VAR 
+ I: INTEGER;
+BEGIN{ReadNumber}
+  WHILE NOT(EOLN(F)) AND (N >= 0)
   DO
-    BEGIN
+    BEGIN        
       ReadDigit(F, I);
-      IF (Ch <> 'E')
+      IF (I <> -1)
       THEN
-        BEGIN
-          N := N*10 + I;
-          IF (N > 32767)
-          THEN
-            BEGIN
-              Over := TRUE;
-              N := -1
-            END  
-        END
-      ELSE  
-        BEGIN
-          Inv := TRUE;
-          N := 0
-        END      
+        BEGIN     
+          IF (N <= MAXINT div 10) 
+          THEN    
+            BEGIN     
+              N := N * 10;
+              IF(MAXINT - N >= I)
+              THEN
+                N := N + I
+              ELSE
+                N := -1
+            END                                            
+          ELSE
+            N := -1            
+        END     
     END
-END;
-BEGIN
+END;{ReadNumber}
+BEGIN{ReadNumberApp}
+  WRITELN(MAXINT);
   ReadNumber(INPUT, Num); 
-  IF Inv = TRUE
-  THEN
-    WRITELN('Invalid data') 
-  ELSE
-    IF Over = TRUE                         
-    THEN                                                  
-      WRITELN('Overflow');
+  IF Num = -1                    
+  THEN                                                  
+    WRITELN('Overflow');
   WRITELN('Number is ', Num)   
-END.      
+END.{ReadNumberApp}      
