e12f20c03f77b7963b33698eec9c3e2e25b88f5f
algolGuru <61578859+algolGuru@users.noreply.github.com>
2020-04-18 02:59:16 +0300

    ИсправилЗамечания

--- hometask/191.pas
+++ hometask/191.pas
@@ -3,45 +3,57 @@ CONST
   Max = 100;
   Min = 2;
 VAR
-  Sieve: SET OF  2 .. Max;
-  X, Y, Temp: INTEGER;
-BEGIN
+  Sieve: SET OF  Min .. Max;
+  X, Y: INTEGER;
+{========}  
+PROCEDURE WriteSimpleNumbersSieve(VAR SieveWindow: INTEGER);
+BEGIN {WriteSimpleNumbersSieve}
+  WRITELN('Simple numbers in interval is: ');  
+  SieveWindow := Min;  
+  WHILE SieveWindow <> Max + 1 
+  DO
+    BEGIN
+      IF SieveWindow IN Sieve
+      THEN
+        WRITE(SieveWindow,' ');
+      SieveWindow := SieveWindow + 1  
+    END
+END;{WriteSimpleNumbersSieve}
+{========}
+PROCEDURE WriteSieve(VAR SieveWindow: INTEGER);
+BEGIN{WriteSieve}
+  WRITELN('Started interval is: ');  
+  WHILE SieveWindow <> Max + 1
+  DO
+    BEGIN
+      WRITE(SieveWindow, ' ');
+      SieveWindow := SieveWindow + 1
+    END;
+  SieveWindow := Min; 
+  WRITELN 
+END;{WriteSieve}
+PROCEDURE FindSimpleNumbers(VAR SieveWindowX, SieveWindowY: INTEGER);
+BEGIN{FindSimpleNumbers}
+  WHILE SieveWindowX <= SQRT(Max)
+  DO
+    BEGIN
+      SieveWindowY := SieveWindowX;
+      WHILE SieveWindowY < Max + 1 
+      DO 
+        BEGIN
+          IF (SieveWindowY MOD SieveWindowX = 0) AND (SieveWindowY <> SieveWindowX) 
+          THEN
+            Sieve := Sieve - [SieveWindowY]; 
+          SieveWindowY := SieveWindowY + SieveWindowX                     
+        END;  
+      SieveWindowX := SieveWindowX + 1  
+    END;
+END;{FindSimpleNumbers}       
+BEGIN{Prime}
   Sieve := [Min .. Max];
   X := Min;
   Y := Min;
-  Temp := Min;
-  WRITELN('Started interval is: ');  
-  WHILE X <> Max + 1
-  DO
-    BEGIN
-      WRITE(X, ' ');
-      X := X + 1
-    END;
-  X := Min; 
-  WRITELN; 
-  WHILE X <= SQRT(Max)
-  DO
-    BEGIN
-      Y := X;
-      WHILE Y < Max + 1 
-      DO 
-        BEGIN
-          IF (Y MOD X = 0) AND (Y <> X) 
-          THEN
-            Sieve := Sieve - [Y];
-          WRITELN(Y);  
-          Y := Y + X                     
-        END;  
-      X := X + 1  
-    END;
-  WRITELN('Simple numbers in interval is: ');  
-  X := Min;  
-  WHILE X <> Max + 1 
-  DO
-    BEGIN
-      IF X IN Sieve
-      THEN
-        WRITE(X,' ');
-      X := X + 1  
-    END; 
-END.          
+  WriteSieve(X);
+  FindSimpleNumbers(X, Y);
+  WriteSimpleNumbersSieve(X)
+END.{Prime}          
