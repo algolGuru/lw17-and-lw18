9021e46be64084e678d71a8a192f5ae97102e7a3
algolGuru <61578859+algolGuru@users.noreply.github.com>
2020-04-19 16:38:22 +0300

    Исправил 18.2

--- 182.pas
+++ 182.pas
@@ -9,58 +9,83 @@ VAR
   Student: 0 .. ClassSize;
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER;
+  Error: BOOLEAN;
+  NameFile: TEXT;
+PROCEDURE ReadName(VAR Fin, Fout: TEXT);
+VAR
   Ch: CHAR;
-  Fale: BOOLEAN;
-  F: TEXT;
+BEGIN {ReadName}
+  WRITELN('Enter the surname of student number ', Student + 1);
+  REWRITE(Fout);
+  IF NOT EOLN
+  THEN
+    WHILE (NOT EOLN(Fin)) AND (NOT Error)
+    DO
+      BEGIN
+        IF NOT(EOLN)
+        THEN
+          READ(Fin, Ch);  
+        IF (Ch <> ' ')   
+        THEN
+          IF (NOT(Ch < 'A')) OR ((Ch > 'z'))
+          THEN
+            WRITE(Fout, Ch)
+          ELSE
+            Error := TRUE  
+      END
+  ELSE
+    Error := TRUE;    
+  RESET(Fin)
+END;{ReadName}
+PROCEDURE WriteName(VAR Fin, Fout: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN {WriteName}
+  RESET(Fin);
+  WHILE NOT EOLN(Fin) AND (NOT Error)
+  DO 
+    BEGIN
+      READ(Fin, Ch);
+      WRITE(Fout, Ch)       
+    END
+END;{WriteName}  
 BEGIN {AverageScore}
   ClassTotal := 0;
   WRITELN('Student averages:');
   Student := 0;
-  WHILE Student < ClassSize
+  WHILE (Student < ClassSize) AND (NOT Error)
   DO 
     BEGIN
-      Fale := FALSE;
+      Error := FALSE;
       TotalScore := 0;
       WhichScore := 0;
-      WRITELN('Enter the surname of student number ', Student + 1);
-      REWRITE(F);
-      WHILE NOT EOLN(INPUT)
-      DO
+      ReadName(INPUT, NameFile);
+      IF (NOT Error)
+      THEN 
         BEGIN
-          READ(INPUT, Ch);
-          IF Ch <> ' '    
-          THEN
-            WRITE(F, Ch)
-        END;   
-      RESET(INPUT);  
-      WRITELN('Enter grades of the student''s number ', Student + 1);
-      WHILE WhichScore < 4
-      DO
-        BEGIN   
-          IF (NOT EOLN)
-          THEN
-            READ(NextScore);
-          IF (NextScore < 0) OR (NextScore > 100)   
-          THEN
-            Fale := TRUE;  
-          TotalScore := TotalScore + NextScore;
-          WhichScore := WhichScore +1;
-        END;           
-      READLN;
+          WRITELN('Enter grades of the student''s number ', Student + 1);  
+          WHILE (WhichScore < 4) AND (NOT Error) 
+          DO
+            BEGIN   
+              IF (NOT EOLN)
+              THEN
+                READ(NextScore)
+              ELSE
+                IF (WhichScore <> 4)
+                THEN
+                  Error := TRUE;           
+              IF (NextScore < 0) OR (NextScore > 100)  
+              THEN
+                Error := TRUE;  
+              TotalScore := TotalScore + NextScore;
+              WhichScore := WhichScore +1
+            END;           
+          READLN
+      END; 
       TotalScore := TotalScore * 10;
       Ave := TotalScore DIV NumberOfScores;
-      RESET(F);
-      WHILE NOT EOLN(F)
-      DO 
-        BEGIN
-          READ(F, Ch);
-          IF ((Ch < 'A') OR (Ch > 'z')) AND (Ch <> ' ')
-          THEN
-            Fale := TRUE     
-          ELSE
-            WRITE(OUTPUT, Ch)       
-        END; 
-      IF Fale = FALSE
+      WriteName(NameFile, OUTPUT);      
+      IF Error = FALSE
       THEN    
         IF (Ave MOD 10) >= 5
         THEN
@@ -70,13 +95,14 @@ BEGIN {AverageScore}
       ELSE
         WRITELN('Incorrect Data');      
       ClassTotal := ClassTotal + TotalScore;
-      Student := Student + 1;
+      Student := Student + 1
     END;
-  WRITELN;
-  WRITELN ('Class average: ');
-  ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
-  WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
-END.  {AverageScore}
-
-        
-              
+  IF NOT Error
+  THEN
+  BEGIN
+    WRITELN;
+    WRITELN ('Class average: ');
+    ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
+    WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10 : 1)
+  END  
+END.  {AverageScore}                     
