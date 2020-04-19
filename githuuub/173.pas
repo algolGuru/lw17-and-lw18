bf9c0f74a3d29ea3e20e7b0574a7fa84626fb833
algolGuru <61578859+algolGuru@users.noreply.github.com>
2020-04-19 14:36:30 +0300

    Исправлено

--- githuuub/173.pas
+++ githuuub/173.pas
@@ -9,17 +9,19 @@ BEGIN{ReadDigit}
   D := -1;{Åñëè íå âñòðåòèòñÿ öèðôà â ôàéëå, òî ïðîöåäóðà âîçâðàùàåò -1}
   IF NOT(EOLN)
   THEN
-    READ(F, Ch);  
-  IF Ch = '1' THEN D := 1 ELSE
-  IF Ch = '2' THEN D := 2 ELSE
-  IF Ch = '3' THEN D := 3 ELSE
-  IF Ch = '4' THEN D := 4 ELSE
-  IF Ch = '5' THEN D := 5 ELSE
-  IF Ch = '6' THEN D := 6 ELSE
-  IF Ch = '7' THEN D := 7 ELSE
-  IF Ch = '8' THEN D := 8 ELSE
-  IF Ch = '9' THEN D := 9 ELSE
-  IF Ch = '0' THEN D := 0;    
+    BEGIN
+      READ(F, Ch);  
+      IF Ch = '1' THEN D := 1 ELSE
+      IF Ch = '2' THEN D := 2 ELSE
+      IF Ch = '3' THEN D := 3 ELSE
+      IF Ch = '4' THEN D := 4 ELSE
+      IF Ch = '5' THEN D := 5 ELSE
+      IF Ch = '6' THEN D := 6 ELSE
+      IF Ch = '7' THEN D := 7 ELSE
+      IF Ch = '8' THEN D := 8 ELSE
+      IF Ch = '9' THEN D := 9 ELSE
+      IF Ch = '0' THEN D := 0
+    END  
 END;{ReadDigit}  
 PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);{ReadNumber}
 VAR 
